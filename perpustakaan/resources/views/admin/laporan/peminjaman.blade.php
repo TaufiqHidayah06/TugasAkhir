e8e03fc29222cd1ac4d82e833ae0e10046b0cc96
TaufiqHidayah6 <taufiqhidayah0611@gmail.com>
2023-05-06 15:20:47 +0700

    Pembaruan Title Laporan

--- perpustakaan/resources/views/admin/laporan/peminjaman.blade.php
+++ perpustakaan/resources/views/admin/laporan/peminjaman.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Admin Laporan</title>
+    <title>Laporan Peminjaman</title>
 
     <link rel="shortcut icon" type="image/png" href="{{asset('img/icon.png')}}">
     <!-- Google Font: Source Sans Pro -->
